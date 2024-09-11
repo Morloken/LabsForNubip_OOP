ba3ca9d8d054847f3c610df031a925feed7aa516
Morlok <nikitaillarionov2018@gmail.com>
2024-09-11 22:09:04 +0300

    Lab1 full completed

---
+++ lab1.cpp
@@ -0,0 +1,82 @@
+//Варіант 10. Створити клас «ромб»:
+// a) його екземпляр містить розміри його діагоналей a і b.
+// b) його конструктор без параметра створює екземпляр зі значенням 0, а
+// конструктор з параметрами створює екземпляр з відповідним значенням a і b. c)
+// його методи дозволяють отримувати і присвоювати значення a, b і обчислювати
+// значення площі. d) функція print_sqrt () виводить на екран значення a, b і
+// площі цієї фігури.
+
+#include <cmath>
+#include <iostream>
+#include <windows.h>
+
+
+using namespace std;
+
+class Rhombus {
+private:
+  double a;
+  double b;
+
+public:
+  Rhombus() : a(0), b(0) {
+    cout << "Ромб створений з діагоналями за замовчуванням." << endl;
+    cout << "--------------" << endl;
+  }
+
+  Rhombus(double a_val, double b_val) : a(a_val), b(b_val) {
+    cout << "Ромб створений з діагоналями a: " << a << ", b: " << b << "."
+         << endl;
+  }
+
+  ~Rhombus() { // destructor
+    cout << "Ромб з діагоналями a: " << a << " і b: " << b << " знищено."
+         << endl;
+  }
+
+  void setA(double a_val) { a = a_val; }
+
+  void setB(double b_val) { b = b_val; }
+
+  double getA() const { return a; } // method that doesnt change the value
+
+  double getB() const { return b; } // method that doesnt change the value
+
+  double calculateArea() const {
+    return (a * b) / 2;
+  }
+
+  void print_sqrt() const {
+    cout << "--------------" << endl;
+    cout << "Функція print_sqrt() виводить на екран значення: " << endl;
+    cout << "Діагональ a: " << a << ", Діагональ b: " << b << endl;
+    cout << "Площа ромба: " << calculateArea() << endl;
+  }
+}; //---------------- end of class Rhombus
+
+int main() {
+    SetConsoleOutputCP(1251);
+        SetConsoleCP(1251);
+  double a_input, b_input;
+  cout << "Введіть значення для діагоналі a: ";
+  cin >> a_input;
+  cout << "Введіть значення для діагоналі b: ";
+  cin >> b_input;
+
+  Rhombus rhombus1;
+  Rhombus rhombus2(a_input, b_input);
+  cout << "===================" << endl;
+  cout << "Введіть нове значення для діагоналі a: ";
+  cin >> a_input;
+  rhombus1.setA(a_input);
+
+  cout << "Введіть нове значення для діагоналі b: ";
+  cin >> b_input;
+  rhombus1.setB(b_input);
+  cout << "===================" << endl;
+  rhombus1.print_sqrt();
+  rhombus2.print_sqrt();
+
+  return 0;
+}
+

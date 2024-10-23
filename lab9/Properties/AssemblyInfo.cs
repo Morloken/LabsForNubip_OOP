52fdaffb81adebbd9ba197e29a97363342a3d115
Morlok <nikitaillarionov2018@gmail.com>
2024-10-24 00:52:27 +0300

    Added main code for lab9

--- lab9/Properties/AssemblyInfo.cs
+++ lab9/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("lab9")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MorlokIndustries")]
 [assembly: AssemblyProduct("lab9")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]

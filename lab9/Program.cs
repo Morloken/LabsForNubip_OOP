30599d35ccad1b1c592021a8129c9ce28bf7621b
Morlok <nikitaillarionov2018@gmail.com>
2024-10-23 23:13:37 +0300

    Created placeholder for lab9, some testing

---
+++ lab9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введiть своє iм'я:"); 
+            string name = Console.ReadLine(); // вводимо ім'я
+            Console.WriteLine($"Привiт,{name}"); // виводимо ім'я на консоль
+            Console.ReadKey(); 
+        }
+    }
+}

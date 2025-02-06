ec691834f262734e125150095f84bb187e7b9db9
Morlok <nikitaillarionov2018@gmail.com>
2025-02-06 19:47:56 +0200

    Created prebuild for future Lab10, strucktuarised file repo

---
+++ lab10/Exersice_1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
---
+++ lab10/Exersice_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 2, World!");
+
+        }
+    }
+}

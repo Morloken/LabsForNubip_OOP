8b300411252a8a97e9146ca737b50f306e9dc8d9
Illarionov Mykyta <nikitaillarionov2018@gmail.com>
2025-03-28 13:01:12 +0200

    made an half of the lab15

---
+++ lab15_NET/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace lab15_NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxH = new System.Windows.Forms.TextBox();
+            this.textBoxL = new System.Windows.Forms.TextBox();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonClose_Click = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxH
+            // 
+            this.textBoxH.Location = new System.Drawing.Point(159, 16);
+            this.textBoxH.Name = "textBoxH";
+            this.textBoxH.Size = new System.Drawing.Size(217, 22);
+            this.textBoxH.TabIndex = 0;
+            // 
+            // textBoxL
+            // 
+            this.textBoxL.Location = new System.Drawing.Point(159, 64);
+            this.textBoxL.Name = "textBoxL";
+            this.textBoxL.Size = new System.Drawing.Size(217, 22);
+            this.textBoxL.TabIndex = 1;
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(159, 107);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(217, 22);
+            this.textBoxR.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "h (висота)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "l (твірна)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "r (радіус основи)";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(8, 196);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(188, 16);
+            this.labelResult.TabIndex = 6;
+            this.labelResult.Text = "Відображення результатів: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 32);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Обчислити";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonClose_Click
+            // 
+            this.buttonClose_Click.BackColor = System.Drawing.Color.LightCoral;
+            this.buttonClose_Click.Location = new System.Drawing.Point(397, 3);
+            this.buttonClose_Click.Name = "buttonClose_Click";
+            this.buttonClose_Click.Size = new System.Drawing.Size(85, 32);
+            this.buttonClose_Click.TabIndex = 9;
+            this.buttonClose_Click.Text = "Закрити";
+            this.buttonClose_Click.UseVisualStyleBackColor = false;
+            this.buttonClose_Click.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 527);
+            this.Controls.Add(this.buttonClose_Click);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxR);
+            this.Controls.Add(this.textBoxL);
+            this.Controls.Add(this.textBoxH);
+            this.Name = "Form1";
+            this.Text = "ConeCalculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxH;
+        private System.Windows.Forms.TextBox textBoxL;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonClose_Click;
+    }
+}
+
---
+++ lab15_NET/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab15_NET
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Центрування
+            int screenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            int formWidth = 400;
+            int formHeight = 300;
+
+            this.Width = formWidth;
+            this.Height = formHeight;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Left = (screenWidth - formWidth) / 2;
+            this.Top = (screenHeight - formHeight) / 2;
+
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Text = "Конус – Обчислення S та V";
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.FormBorderStyle = FormBorderStyle.None;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ControlBox = false;
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+            
+        private void button1_Click(object sender, EventArgs e)//обчислити кнопка
+        {
+         
+            try
+            {
+                double h = double.Parse(textBoxH.Text);
+                double l = double.Parse(textBoxL.Text);
+                double r = double.Parse(textBoxR.Text);
+
+                double S = Math.PI * r * l;
+                double V = Math.PI * Math.Pow(r, 2) * h / 3;
+
+                labelResult.Text = $"Площа бічної поверхні:\n S = {S:F2}\nОб’єм V = {V:F2}";
+            }
+            catch
+            {
+                MessageBox.Show("Будь ласка, введіть правильні числові значення.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        
+
+        }
+
+        private void button2_Click  (object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
